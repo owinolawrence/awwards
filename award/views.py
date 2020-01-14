9b060f5081f2670de4ccada2d63d389d69236436
owinolawrence <owinolawrence21@gmail.com>
2020-01-14 17:28:33 +0300

    create search results definition

--- award/views.py
+++ award/views.py
@@ -7,3 +7,19 @@ from django.http import HttpResponse
 
 def index(request):
     return render( request , 'index.html')
+
+
+
+
+def search_results(request):
+
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
