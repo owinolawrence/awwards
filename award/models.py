2accd8c35ec3c40c7b82398880cdc5924bde37f6
owinolawrence <owinolawrence21@gmail.com>
2020-01-14 15:03:44 +0300

    create procfile,.env,.gitignore, and runtime.txt

--- award/models.py
+++ award/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title =models.CharField(max_length =30)
+    project_image=models.ImageField(default = 'default.jpg', upload_to="Pictures")
+    description=models.TextField()
+    project_url=models.TextField()
+
+    def __str__(self):
+        return self.caption
+
+
+    # @classmethod
+    # def search_by_title(cls,search_term):
+    #     award = cls.object
--- award/urls.py
+++ award/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns=[
     url('',views.index, name= 'index'),
+    # url(r'^search/', views.search_results, name='search_results'),
 ]
