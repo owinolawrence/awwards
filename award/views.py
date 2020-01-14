22319e10032e7ce53e2b3a7e2c829424f282df0f
owinolawrence <owinolawrence21@gmail.com>
2020-01-14 17:56:42 +0300

    add styling to body

--- award/views.py
+++ award/views.py
@@ -22,4 +22,4 @@ def search_results(request):
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-news/search.html',{"message":message})
+        return render(request, 'search.html',{"message":message})
