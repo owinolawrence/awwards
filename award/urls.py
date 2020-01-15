281518bb359db7dd038d98012fdd25197bba9309
owinolawrence <owinolawrence21@gmail.com>
2020-01-15 13:19:38 +0300

    add link urls for tempelates

--- award/urls.py
+++ award/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 
 
 from . import views 
@@ -7,4 +7,10 @@ from . import views
 urlpatterns=[
     url('',views.index, name= 'index'),
     url(r'^search/', views.search_results, name='search_results'),
+    url('detail/<int:pk>/',views.PostDetailView.as_view(), name ='post-detail'),
+    url('post/new/',views.PostCreateView.as_view(), name ='post-create'),
+    url (r'^search/',views.search_results,name= 'search_results'),    
+    url('api-auth/',include('rest_framework.urls', namespace='rest_framework')),
+    url('post/review/(?P<post_id>\d+)',views.rate, name ='post-review'),    
+
 ]
